871a2cafd535ce3bc078e80229d993159f0c6e89
Gabriel123789 <bispogabriel398@gmail.com>
2026-05-12 13:42:35 -0300

    Add files via upload

---
+++ PW I /curriculo/JS/script.js
@@ -0,0 +1,16 @@
+const botao = document.getElementById("tema");
+        // carrgar preferência
+        if (localStorege.getItem("tema") === "dark") {
+            document.body. classList.add("dark");
+        }
+
+        // alternar tema
+        botao. addEventListener("click", () => {
+            document.body.classList.toggle("dark");
+
+            if (document.body.classList.contains("dark")) {
+                localStorage.setItem("tema", "dark");
+            } else {
+                localStorage.setItem("tema", "light");
+            }
+        });
