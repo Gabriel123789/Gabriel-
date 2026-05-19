34b9303d6da426fefdf36136bf7238fe458971ed
Gabriel123789 <bispogabriel398@gmail.com>
2026-05-19 13:41:02 -0300

    Add files via upload

--- PW I /curriculo/JS/script.js
+++ PW I /curriculo/JS/script.js
@@ -1,16 +1,48 @@
+// const botao = document.getElementById("tema");
+//         // carrgar preferência
+//         if (localStorege.getItem("tema") === "dark") {
+//             document.body. classList.add("dark");
+//         }
+
+//         // alternar tema
+//         botao. addEventListener("click", () => {
+//             document.body.classList.toggle("dark");
+
+//             if (document.body.classList.contains("dark")) {
+//                 localStorage.setItem("tema", "dark");
+//             } else {
+//                 localStorage.setItem("tema", "light");
+//             }
+//         });
+
+
 const botao = document.getElementById("tema");
-        // carrgar preferência
-        if (localStorege.getItem("tema") === "dark") {
-            document.body. classList.add("dark");
+
+const updateButtonIcon = () => {
+    if (!botao) return;
+    const darkmode = document.body.classList.contains("dark"); classList.contains("dark");
+    botao.textocontent = darkmode ? "☀️": "🌙";
+    botao.title = darkmode ? "mudar para modo claro" : "mudar para modo escuro";
+};
+
+const temaAtual = localStorage.getItem("tema") === "dark" ? "dark" : "light";
+if (temaAtual === "dark") {
+    document.body.classList,add("dark");
+}
+
+updateButtonIcon();
+
+if (botao) {
+    botao.addEventListene("click", () => {
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("tema", "dark");
+        
+        } else {
+            localStorage.setItem("tema", "light");
         }
 
-        // alternar tema
-        botao. addEventListener("click", () => {
-            document.body.classList.toggle("dark");
-
-            if (document.body.classList.contains("dark")) {
-                localStorage.setItem("tema", "dark");
-            } else {
-                localStorage.setItem("tema", "light");
-            }
-        });
+        updateButtonIcon();
+    });
+}
